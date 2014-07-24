4bdef6872788385938f00dede0406ba2e6258525
Amit <jakubowicz.amit@gmail.com>
2014-07-24 15:31:38 +0200

    Updates

--- src/app/app.js
+++ src/app/app.js
@@ -1,30 +1,62 @@
 'use strict';
 
-angular.module('happyAppy', ['ngRoute'])
+angular.module('happyAppy', ['ui.router'])
 
   .constant('AppConfig', {
     attributeState: 'happy'
   })
 
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/smile', {
-        controller: 'MainCtrl',
-        templateUrl: 'app/app.tpl.html'
+  .config(function($stateProvider, $urlRouterProvider) {
+//    $routeProvider
+//      .when('/smile', {
+//        controller: 'MainCtrl',
+//        templateUrl: 'app/app.tpl.html'
+//      })
+//      .when('/not-found', {
+//        controller: 'SadCtrl',
+//        templateUrl: 'app/not-found.tpl.html'
+//      })
+//      .otherwise({
+//        redirectTo: '/not-found'
+//      })
+
+
+    $urlRouterProvider.otherwise('/appy/choose');
+
+    $stateProvider
+      .state('app', {
+        url: '/appy',
+        templateUrl: 'app/app.tpl.html',
+        controller: 'MainCtrl'
       })
-      .when('/not-found', {
-        controller: 'SadCtrl',
-        templateUrl: 'app/not-found.tpl.html'
+      .state('app.choose', {
+        url: '/choose',
+        templateUrl: "app/choose.tpl.html",
+        controller: 'ChooseCtrl'
       })
-      .otherwise({
-        redirectTo: '/not-found'
+      .state('app.happy', {
+        url: "/happy",
+        templateUrl: "app/happy.tpl.html",
+        controller: 'HappyCtrl'
+      })
+      .state('app.sad', {
+        url: '/sad',
+        templateUrl: 'app/sad.tpl.html',
+        controller: 'SadCtrl'
       })
   })
-
-  .controller('MainCtrl', function($scope, $routeParams, $rootScope, AppConfig) {
+  .controller('MainCtrl', function($scope, $rootScope, AppConfig, $location) {
+    console.log('main ctrl');
     $scope.state = AppConfig.attributeState;
+    $location.url('/appy/choose');
   })
-
+  .controller('ChooseCtrl', function($scope) {
+    console.log('choose ctrl');
+  })
+  .controller('HappyCtrl', function($scope) {
+    console.log('happy ctrl');
+  })
   .controller('SadCtrl', function($scope) {
+    console.log('sad ctrl');
 
   });
--- src/templates.js
+++ src/templates.js
@@ -2,12 +2,41 @@ angular.module('happyAppy').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('app/app.tpl.html',
-    "<h2>sonrisa (;</h2>\n"
+    "<core-header-panel>\n" +
+    "  <core-toolbar id=\"mainheader\">\n" +
+    "    <paper-icon-button id=\"navicon\"\n" +
+    "                       icon=\"arrow-back\"></paper-icon-button>\n" +
+    "    <span flex>Title</span>\n" +
+    "    <paper-icon-button id=\"searchbutton\"\n" +
+    "                       icon=\"search\"></paper-icon-button>\n" +
+    "  </core-toolbar>\n" +
+    "\n" +
+    "  <div ui-view></div>\n" +
+    "\n" +
+    "</core-header-panel>\n"
+  );
+
+
+  $templateCache.put('app/choose.tpl.html',
+    "<p>choose mood:\n" +
+    "<a href=\"#/appy/happy\">happy</a>\n" +
+    "<a href=\"#/appy/sad\">sad</a>\n" +
+    "</p>\n"
+  );
+
+
+  $templateCache.put('app/happy.tpl.html',
+    "<p>happy</p>\n"
   );
 
 
   $templateCache.put('app/not-found.tpl.html',
     "<h2>sad ): <a href=\"#/smile\"> better smile</a></h2>\n"
+  );
+
+
+  $templateCache.put('app/sad.tpl.html',
+    "<p>sad</p>\n"
   );
 
 }]);
